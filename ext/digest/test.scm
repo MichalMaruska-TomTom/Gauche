38acf99f1f12f4f6a71c77238d80f51f1bfcca54
Michal Maruska <mmaruska@gmail.com>
2014-06-28 13:45:52 +0200

    fix aborted tests -- avoid them?

--- ext/digest/test.scm
+++ ext/digest/test.scm
@@ -10,7 +10,13 @@
        (digest-hexify '#u8(1 #x23 #x45 #x67 #x89 #xab #xcd #xef)))
 
 (include "test-md5")
-(include "test-sha")
-(include "test-hmac")
+(test-end)
 
+
+(test-start "digest sha")
+(include "test-sha")
+(test-end)
+
+(test-start "digest hmac")
+(include "test-hmac")
 (test-end)
